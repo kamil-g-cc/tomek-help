b4e6bb1b08aa0d443c5cb55ed97630bd5c3186e6
Kamil Grondys <kamil.grondys@codecool.com>
2020-04-20 22:24:18 +0200

    init repo
    Dodanie pliku Program.cs z przykladem zastosowania przekazywania
    argumentu jako referencji:
    https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/refhttps://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/ref

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tomek_help
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Method(ref int refArgument)
+            {
+                refArgument = refArgument + 1;
+				System.Console.WriteLine(refArgument);
+            }
+
+            int number = 1;
+            Method(ref number);
+            Console.WriteLine(number);
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
